c9a782be06a099897c617f3d06bf905ac21c496f
Abimael Santiago <abimaelsv6@gmail.com>
2019-05-20 19:54:19 -0300

    gráfico

--- src/components/constantes/constantes.ts
+++ src/components/constantes/constantes.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: 'constantes.html'
 })
 export class ConstantesComponent {
-  url: string = 'http://localhost:3000/api/v1/';
-  //url: string = 'http://162.243.161.30:3016/api/v1/';
+  //url: string = 'http://localhost:3000/api/v1/';
+  url: string = 'http://162.243.161.30:3016/api/v1/';
 
   constructor() {
   }
